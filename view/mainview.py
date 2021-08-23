64d5ab5b679add8042b19605e87dbd5b4e955303
Justin Schuster <schujustin@gmail.com>
2021-08-23 14:57:15 -0400

    Added percent symbols and currency signs.

--- view/mainview.py
+++ view/mainview.py
@@ -28,16 +28,16 @@ class CrypytoUI(QWidget):
         self.show()
 
     def createTable(self):
-        ''' Method to create the table. '''
+        ''' Method to create the table. Will be used for Watch List. '''
         self.tableWidget = QTableWidget()
         self.tableWidget.setRowCount(len(data))
         self.tableWidget.setColumnCount(3)
         self.tableWidget.setHorizontalHeaderLabels(['Name', 'Price', '24hr G/L %'])
+
         for i in range(len(data)):
             item_name = QTableWidgetItem(data[i][0])
-            item_price = QTableWidgetItem(data[i][1])
-            item_gl = QTableWidgetItem(data[i][2])
+            item_price = QTableWidgetItem('${}'.format(data[i][1]))
+            item_gl = QTableWidgetItem('{}%'.format(data[i][2]))
             self.tableWidget.setItem(i, 0, item_name)
             self.tableWidget.setItem(i, 1, item_price)
-            self.tableWidget.setItem(i, 2, item_gl) 
-
+            self.tableWidget.setItem(i, 2, item_gl)
