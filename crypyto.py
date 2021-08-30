295504e82c9ca9524b290d8c1fe9ebbaf2ee6793
Justin Schuster <schujustin@gmail.com>
2021-08-30 16:29:27 -0400

    Added docked watch list to ui.

--- crypyto.py
+++ crypyto.py
@@ -3,7 +3,11 @@ import cui.main as cui
 from PyQt5.QtWidgets import QApplication
 
 def main():
-    app = QApplication(sys.argv)
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication(sys.argv)
+    else:
+        print('QApplication instance already exists: %s' % str(app))
     view = cui.CrypytoMainWindow()
     view.show()
     sys.exit(app.exec())
--- cui/main.py
+++ cui/main.py
@@ -15,30 +15,51 @@ from cui.items import DateAxisItem
 class CrypytoMainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
+        self.title = 'Crypyto'
+        self.width = 1200
+        self.height = 900
+        self.left = 0
+        self.top = 500
 
         try:
-            self.setupLayout()
             self.setupUI()
         except Exception as e:
             print('Failed to create MainWindow:', str(e)) 
             sys.exit(-1)
 
     def setupLayout(self):
-        #self.grid = QGridLayout()
-        #self.setLayout(self.grid)
         pass
 
     def setupUI(self) -> None:
         self.setupChart()
 
     def setupChart(self) -> None:
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        layout = QHBoxLayout()  
+
+        bar = self.menuBar()
+        file = bar.addMenu("File")
+        file.addAction('About')
+        file.addAction('Quit')
+
         self.plotWidget = PlotWidget()
-        self.setCentralWidget(self.plotWidget)
-        
+        layout.addWidget(self.plotWidget)
         axis = DateAxisItem(orientation='bottom')
         axis.attachToPlotItem(self.plotWidget.getPlotItem())
-
         self.time_data, self.price_data = tickers.fetchChartData('BTC/USD', '1m')
         self.plotWidget.plot(x=self.time_data, y=self.price_data)
         self.plotWidget.getPlotItem().setTitle('BTC/USD')
-        self.plotWidget.getPlotItem().showGrid(x=True, y=True, alpha=0.2)  
+        self.plotWidget.getPlotItem().showGrid(x=True, y=True, alpha=0.2)     
+
+        self.items = QDockWidget("Watch List", self)
+        self.listWidget = QListWidget()
+        self.listWidget.addItem('item1')
+        self.listWidget.addItem('item2')
+        self.items.setWidget(self.listWidget)
+        self.items.setFloating(False)
+
+        self.setCentralWidget(self.plotWidget)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.items)
+        self.setLayout(layout)
